9d5acff3f56c9d9e64053d4fadd9c7a5226bd7e5
pedrodelyra <pedrodelyra@gmail.com>
2014-09-20 15:53:07 -0300

    Criacao das models basicas

---
+++ MeG/source/br/org/meg/model/Estado.java
@@ -0,0 +1,32 @@
+package br.org.meg.model;
+
+public class Estado {
+	private int id;
+	private String nome;
+	private String sigla;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+}
---
+++ MeG/source/br/org/meg/model/Quadro.java
@@ -0,0 +1,49 @@
+package br.org.meg.model;
+
+public class Quadro {
+	private int id;
+	private int ano;
+	private float valor;
+	private Estado estado;
+	private Secao secao;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public Secao getSecao() {
+		return secao;
+	}
+
+	public void setSecao(Secao secao) {
+		this.secao = secao;
+	}
+}
