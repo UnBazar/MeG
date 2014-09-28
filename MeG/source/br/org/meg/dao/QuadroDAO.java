ebce8c4ab9c00854d3fe2aaf829af0b899173316
Pedro de lyra + Hugo Catarino <pedrodelyra@gmail.com>
2014-09-28 16:13:15 -0300

    método adicionar implementado

--- MeG/source/br/org/meg/dao/QuadroDAO.java
+++ MeG/source/br/org/meg/dao/QuadroDAO.java
@@ -1,6 +1,8 @@
 package br.org.meg.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import br.org.meg.model.Quadro;
@@ -13,7 +15,20 @@ public class QuadroDAO {
 	}
 	
 	public void adicionar(Quadro quadro) {
-		
+		String sql = "INSERT INTO Quadro(ano, valor, estado_id, secao_id, descricao_id) VALUES(?,?,?,?,?)";
+		try {
+			PreparedStatement stmt = this.connection.prepareStatement(sql);
+			stmt.setInt(1, quadro.getAno());
+			stmt.setFloat(2, quadro.getValor());
+			stmt.setInt(3, quadro.getEstado().getId());
+			stmt.setInt(4, quadro.getSecao().getId());
+			stmt.setInt(5, quadro.getDescricao().getId());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException sqlException) {
+			System.err.println(sqlException);
+			throw new RuntimeException("Erro ao acessar o banco de dados!");
+		}
 	}
 	
 	public void remover(Quadro quadro) {
