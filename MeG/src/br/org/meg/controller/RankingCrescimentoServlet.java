66a8db62df3b71e5c793003ff931509c3db1cb76
Hugo Catarino <hugocatarino1@gmail.com>
2014-11-16 22:06:24 -0200

    Implementando métodos do ranking de crescimento

--- MeG/src/br/org/meg/controller/RankingCrescimentoServlet.java
+++ MeG/src/br/org/meg/controller/RankingCrescimentoServlet.java
@@ -1,6 +1,7 @@
 package org.meg.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.meg.dao.QuadroDAO;
+import org.meg.dao.UtilDAO;
 import org.meg.model.Descricao;
 import org.meg.model.Quadro;
 import org.meg.model.Secao;
@@ -30,6 +32,97 @@ public class RankingCrescimentoServlet extends HttpServlet {
 		secao.setId(secao_id);
 		descricao.setId(descricao_id);
 		List<Quadro> listaInicial = dao.obterLista(anoInicial, secao, descricao);
+		List<Quadro> listaFinal = dao.obterLista(anoFinal, secao, descricao);
+		List<Quadro> listaCrescimento = obterListaCrescimento(listaInicial,listaFinal);
+		this.ordenacaoCrescente(listaCrescimento);
+		if(descricao.getId() == 5){
+			this.alterarSalario(listaInicial, anoInicial);
+			this.alterarSalario(listaFinal, anoFinal);
+		}
+		request.setAttribute("listaCrescimento", listaCrescimento);
+		request.setAttribute("anoInicial", anoInicial);
+		request.setAttribute("anoFinal", anoFinal);
+		request.setAttribute("descricao", descricao.getNome());
+		request.setAttribute("setor", secao.getNome());
+		request.getRequestDispatcher("tabela-crescimento.jsp").forward(request, response);
 	}
-
+	/**
+	 * Método que pega uma lista com o crescimento
+	 * @param listaInicial
+	 * @param listaFinal
+	 * @return uma lista de quadros em que o valor é o crescimento
+	 */
+	private ArrayList<Quadro> obterListaCrescimento(List<Quadro> listaInicial, 
+			List<Quadro> listaFinal){
+		ArrayList<Quadro> listaCrescimento = new ArrayList<Quadro>();
+		for(int i = 0; i < listaInicial.size(); i++){
+			if(i < listaFinal.size()){
+				float crescimento = calculaCrescimento(listaFinal.get(i).getValor(),listaInicial.get(i).getValor());
+				Quadro lista = listaFinal.get(i);
+				lista.setValor(crescimento);
+				listaCrescimento.add(lista);
+			}
+		}
+		return listaCrescimento;
+	}
+	/**
+	 * Método que calcula o crescimento em porcentagem
+	 * @param valorFinal
+	 * @param valorInicial
+	 * @return o crescimento percentual de dois valores
+	 */
+	private float calculaCrescimento(float valorFinal, float valorInicial){
+		float resultado = 0;
+		resultado = (valorInicial / valorFinal) * 100;
+		return resultado;
+	}
+	/**
+	 * Método responsável por ordenar a lista de salário médio em ordem crescente
+	 * @param lista de salários médios
+	 */
+	private void ordenacaoCrescente(List<Quadro> lista) {
+		int tamanho = lista.size();
+		int maiorElemento;
+		for (int i = 0; i < tamanho - 1; i++) {
+			maiorElemento = i;
+			for (int j = i + 1; j < lista.size(); j++) {
+				if (lista.get(maiorElemento).getValor() < lista.get(j).getValor()) {
+					maiorElemento = j;
+				}
+			}
+			troca(lista, i, maiorElemento);
+		}
+	}
+	
+	/**
+	 * Realiza uma troca entre dois elementos da lista
+	 * @param lista que possui os elementos que serão trocados
+	 * @param index1 índice do primeiro elemento 
+	 * @param index2 índice do segundo elemento
+	 */
+	private void troca(List<Quadro> lista, int index1, int index2) {
+		Quadro buffer = lista.get(index1);
+		lista.set(index1, lista.get(index2));
+		lista.set(index2, buffer);
+	}
+	private void alterarSalario(List<Quadro> lista, int ano) {
+		int tamanho = lista.size();
+		float numeroDeSalarios;
+		float salarioMinimo = getSalarioMinimo(ano);
+		for (int i = 0; i < tamanho; i++) {
+			numeroDeSalarios = lista.get(i).getValor();
+			lista.get(i).setValor(salarioMinimo * numeroDeSalarios);
+		}
+	}
+	
+	/**
+	 * Seleciona o valor do salário mínimo de acordo com o ano especificado
+	 * @param ano
+	 * @return o valor do salário mínimo no ano especificado
+	 */
+	private float getSalarioMinimo(int ano) {
+		UtilDAO dao = new UtilDAO();
+		float salarioMinimo = dao.getSalarioMinimo(ano);
+		return salarioMinimo;
+	}
 }
---
+++ MeG/src/main/webapp/js/tabela-crescimento.jsp
@@ -0,0 +1,47 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page isELIgnored="false" %>
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="js/tabela.js" type="text/javascript"></script>
+		<!-- Style geral -->
+		<link rel="stylesheet" href="css/style.css">
+		<title>Ranking de crescimento</title>
+	</head>
+	<div class="container">
+		<section id="cabecalho">
+			<h1><a href="">MeG - Mercado em gráfico</a></h1>
+				<ul id="menu">
+					<li><a href="#">Sobre</a></li>
+					<li><a href="#">Como usar</a></li>
+					<li><a href="#">Link</a></li>
+			</ul>
+		</section><br>
+		<h1 style="color: #000000">Ranking de ${descricao.nome} na área de ${setor} - ${anoInicial} - ${anoFinal} </h1>
+		<table border="1" style="width:30%; border: 2px solid #3366FF">
+			<tr>
+				<th style="text-align:center">Ranking</th>
+				<th style="text-align:center">Estado</th>
+				<th style="text-align:center">${descricao.nome}</th>
+			</tr>
+			<c:forEach var="quadro" items="${listaCrescimento}" varStatus="id">
+				<tr>
+					<td>${id.count}º</td>
+					<td>${quadro.estado.nome}</td>
+					<td class="dado">
+						${quadro.valor}
+					</td>	
+				</tr>
+			</c:forEach>
+		</table>
+		</div>
+	</body>
+</html>
