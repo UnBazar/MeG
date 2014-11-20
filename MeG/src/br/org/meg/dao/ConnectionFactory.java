918ea09e46f3422509f8a4ee520009468cafd0fb
Rafael Rabetti <leafar_rabetti@hotmail.com>
2014-11-20 14:15:45 -0200

    mudando connection factory para pegar a url, username e password de arquivo

--- MeG/src/br/org/meg/dao/ConnectionFactory.java
+++ MeG/src/br/org/meg/dao/ConnectionFactory.java
@@ -1,25 +1,33 @@
 package org.meg.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
 	
 	private static Connection connection = createConnection();
-	
-	private static Connection createConnection() {
-		String url = "jdbc:mysql://localhost/MEG";
-		try {
-			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-			return DriverManager.getConnection(url, "root", "root");
-		} catch(SQLException sqlException) {
-			System.err.println(sqlException);
-			throw new RuntimeException("Erro ao estabelecer uma conexão com o bando de dados!");
+
+	private static Connection createConnection(){
+		try{
+	        Properties prop = new Properties();
+	        prop.load(new FileInputStream(System.getProperty("user.home") + "/mydb.cfg"));
+	        String host = prop.getProperty("host").toString();
+	        String username = prop.getProperty("username").toString();
+	        String password = prop.getProperty("password").toString();
+	        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			return DriverManager.getConnection(host, username, password);
+		}catch (IOException | SQLException e) {
+			e.printStackTrace();
 		}
-	}
+        return connection;
+    }
 	
-	public static Connection getConnection() {
+	public static Connection getConnection(){
 		return ConnectionFactory.connection;
 	}
 }
+
