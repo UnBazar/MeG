e79d4591c59822ab78d6f5dd50a1038fb574e1be
pwener <phwener@gmail.com>
2015-04-25 11:05:59 -0300

    Refactoring ConnectionFactory

--- MeG/src/br/org/meg/dao/ConnectionFactory.java
+++ MeG/src/br/org/meg/dao/ConnectionFactory.java
@@ -10,26 +10,42 @@ import java.util.Properties;
 import org.meg.exception.DAOException;
 
 public class ConnectionFactory {
-	
+	// This connection is created when app has begin
 	private static Connection connection = createConnection();
 
-	private static Connection createConnection(){
-		try{
-	        Properties prop = new Properties();
-	        prop.load(new FileInputStream(System.getProperty("user.home") + "/mydb.cfg"));
-	        String host = prop.getProperty("host").toString();
-	        String username = prop.getProperty("username").toString();
-	        String password = prop.getProperty("password").toString();
-	        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+	/**
+	 * Create an connection to work with database
+	 * 
+	 * @return {@link Connection} used in all Data access object
+	 */
+	private static Connection createConnection() {
+		try {
+			// To populate with necessary data to set connection
+			Properties properties = new Properties();
+			// Get path of file config
+			String path = System.getProperty("user.home")+ "/mydb.cfg";
+			// Create an inputStream with path
+			FileInputStream inputStream = new FileInputStream(path);
+			
+			properties.load(inputStream);
+			String host = properties.getProperty("host").toString();
+			String username = properties.getProperty("username").toString();
+			String password = properties.getProperty("password").toString();
+			
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			// return created connection
 			return DriverManager.getConnection(host, username, password);
-		}catch (IOException | SQLException exception) {
-			throw new DAOException("Falha ao criar conexao, a seguinte excecao foi lancada: "
-										+ exception.getMessage(), "ConnectionFactory");
+		} catch (IOException | SQLException exception) {
+			// Registers error in BD if connection has failed
+			throw new DAOException("Failed when initiate App, exception: "
+					+ exception.getMessage(), "ConnectionFactory");
 		}
-    }
+	}
 	
-	public static Connection getConnection(){
+	/**
+	 * @return static connection created one time
+	 */
+	public static Connection getConnection() {
 		return ConnectionFactory.connection;
 	}
 }
-
