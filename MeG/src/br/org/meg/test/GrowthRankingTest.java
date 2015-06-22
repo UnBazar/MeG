18d86e987ba28fc88b7f3f4d84dd7c49ee50c9fb
pedrodelyra <pedrodelyra@gmail.com>
2015-06-22 02:35:56 -0300

    Started to implement Growth Ranking Logic Test

---
+++ MeG/src/br/org/meg/test/GrowthRankingTest.java
@@ -0,0 +1,35 @@
+package org.meg.test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.meg.controller.GrowthRanking;
+
+public class GrowthRankingTest {
+	
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+
+	@Before
+	public void setUp() throws Exception {
+		this.request = mock(HttpServletRequest.class);
+		this.response = mock(HttpServletResponse.class);
+
+		when(request.getParameter("initialYear")).thenReturn("2008");
+		when(request.getParameter("finalYear")).thenReturn("2012");
+		when(request.getParameter("section")).thenReturn("3");
+		when(request.getParameter("description")).thenReturn("5");
+	}
+
+	@Test
+	public void test() {
+		GrowthRanking growthRanking = new GrowthRanking();
+		growthRanking.execute(request, response);
+	}
+
+}
